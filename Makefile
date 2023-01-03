93334e1e3843b8ace643df6c4f0c24b9387d831b
Matei77 <matei.ionescu7@gmail.com>
2023-01-04 01:55:10 +0200

    update lzw run

--- Makefile
+++ Makefile
@@ -1,3 +1,9 @@
+.PHONY: build
+build: lzw
+
+lzw: Lempel-Ziv-Welch/lzw.cpp
+	g++ -Wall -Wextra -std=c++11 -O3 Lempel-Ziv-Welch/lzw.cpp -o Lempel-Ziv-Welch/lzw
+
 .PHONY: run-p1
 run-p1:
 	./run_lzw.sh
@@ -8,4 +14,4 @@ run-p2:
 
 .PHONY: clean
 clean:
-	rm out/* aux/*
+	rm out/* aux/* Lempel-Ziv-Welch/lzw
--- run_lzw.sh
+++ run_lzw.sh
@@ -3,7 +3,7 @@
 echo "Compressing using the LZW algorithm"
 for i in {1..40}
 do
-	./lzw_v5/lzw -c in/test$i.in out/test$i.out1
+	./Lempel-Ziv-Welch/lzw -c in/test$i.in out/test$i.out1
 	echo "compressed test$i.in"
 done
 
@@ -12,7 +12,7 @@ echo ;
 echo "Decompressing using the LZW algorithm"
 for i in {1..40}
 do
-	./lzw_v5/lzw -d out/test$i.out1 aux/aux$i.in1
+	./Lempel-Ziv-Welch/lzw -d out/test$i.out1 aux/aux$i.in1
 	echo "decompressed test$i.out1"
 done
 
