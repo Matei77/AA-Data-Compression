736ffb560b3fbcb1ded282d8588b9cf461ec9972
Matei77 <matei.ionescu7@gmail.com>
2023-01-05 18:31:53 +0200

    update script

--- run_hyperfine.sh
+++ run_hyperfine.sh
@@ -1,17 +1,27 @@
 #! /bin/bash
 
-echo "Comparing compression times."
+echo "----------------------------> Comparing compression times. <----------------------------"
 for i in {01..60}
 do
+	echo ;
+
 	hyperfine -N "./Lempel-Ziv-Welch/lzw -c in/test$i.in out/test$i.out1" "./Huffman/huffman -c in/test$i.in out/test$i.out2"
+
+	echo ;
+	echo '-----------------------------------------------------------------------------------------------------'
 
 done
 
 echo ;
 
-echo "Comparing decompression times."
+echo "----------------------------> Comparing decompression times. <----------------------------"
 for i in {01..60}
 do
+	echo ;
+
 	hyperfine -N "./Lempel-Ziv-Welch/lzw -d out/test$i.out1 aux/aux$i.in1" "./Huffman/huffman -d out/test$i.out2 aux/aux$i.in2"
+
+	echo ;
+	echo '-----------------------------------------------------------------------------------------------------'
 
 done
