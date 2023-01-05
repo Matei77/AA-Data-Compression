4d6b51c5b4230738637baa77516049a58eca82f2
Matei77 <matei.ionescu7@gmail.com>
2023-01-05 15:26:35 +0200

    show compression ratio

--- run_huffman.sh
+++ run_huffman.sh
@@ -1,16 +1,20 @@
 #! /bin/bash
 
 echo "Compressing using the Huffman algorithm"
-for i in {1..40}
+for i in {01..50}
 do
 	./Huffman/huffman -c in/test$i.in out/test$i.out2
-	echo "compressed test$i.in"
+
+	uncompressed_size=$(du -b in/test$i.in | awk '{print $1;}')
+	compressed_size=$(du -b out/test$i.out2 | awk '{print $1;}')
+	
+	awk "BEGIN {printf(\"compressed test$i.in -- compression ratio: %.2f -- space saving ratio: %.2f%%\n\", ($uncompressed_size / $compressed_size), ((1.0 - $compressed_size/$uncompressed_size) * 100.0))}"
 done
 
 echo ;
 
 echo "Decompressing using the Huffman algorithm"
-for i in {1..40}
+for i in {01..50}
 do
 	./Huffman/huffman -d out/test$i.out2 aux/aux$i.in2
 	echo "decompressed test$i.out2"
@@ -19,7 +23,7 @@ done
 echo ;
 
 echo "Checking for differences after decompressing"
-for i in {1..40}
+for i in {01..50}
 do
 	diff -s in/test$i.in aux/aux$i.in2
 done
--- run_lzw.sh
+++ run_lzw.sh
@@ -1,16 +1,20 @@
 #! /bin/bash
 
 echo "Compressing using the LZW algorithm"
-for i in {1..40}
+for i in {01..50}
 do
 	./Lempel-Ziv-Welch/lzw -c in/test$i.in out/test$i.out1
-	echo "compressed test$i.in"
+
+	uncompressed_size=$(du -b in/test$i.in | awk '{print $1;}')
+	compressed_size=$(du -b out/test$i.out1 | awk '{print $1;}')
+
+	awk "BEGIN {printf(\"compressed test$i.in -- compression ratio: %.2f -- space saving ratio: %.2f%%\n\", ($uncompressed_size / $compressed_size), ((1.0 - $compressed_size/$uncompressed_size) * 100.0))}"
 done
 
 echo ;
 
 echo "Decompressing using the LZW algorithm"
-for i in {1..40}
+for i in {01..50}
 do
 	./Lempel-Ziv-Welch/lzw -d out/test$i.out1 aux/aux$i.in1
 	echo "decompressed test$i.out1"
@@ -19,7 +23,7 @@ done
 echo ;
 
 echo "Checking for differences after decompressing"
-for i in {1..40}
+for i in {01..50}
 do
 	diff -s in/test$i.in aux/aux$i.in1
 done
